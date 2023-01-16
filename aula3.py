c6987a67c94a1ded5919bb58707a0032ef945d8c
Cléverton Reis Araújo <122826771+clevertonraraujo@users.noreply.github.com>
2023-01-16 19:11:05 -0300

    aula 3 realizada
    condicionais

---
+++ aula3.py
@@ -0,0 +1,6 @@
+primeiro_valor = input('Digite o primeiro valor: ')
+segundo_valor = input('Digite o segundo valor: ')
+if int (primeiro_valor) > int (segundo_valor):
+    print('O primeiro é maior!')
+else:
+    print('O segundo é maior!')
