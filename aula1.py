1e8570e366989ddffbdae4309d654d27a513c128
Cléverton Reis Araújo <122826771+clevertonraraujo@users.noreply.github.com>
2023-01-16 19:09:36 -0300

    novas aulas realizadas
    variáveis e condicionais

---
+++ aula1.py
@@ -0,0 +1,7 @@
+# Problema01
+# Escreva um programa que retorne o salário/hora
+# com base no salário mensal e carga horaria
+salario_mensal = input('Qual é o seu salário mensal?')
+carga_horaria = input('Qual sua carga horária de trabalho?')
+salario_hora = int (salario_mensal) / int (carga_horaria)
+print (salario_hora)
---
+++ aula2.py
@@ -0,0 +1,6 @@
+numero_atraso = input('quantas vezes você se atrasou?')
+
+if int (numero_atraso) >= 3:
+    print('você não pode entrar')
+else:
+    print('Se liga doidão')
